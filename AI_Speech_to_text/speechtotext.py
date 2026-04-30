c89818495a4b691e8b26e7baca19aa38b62423a7
GOWSHIKA D <gowshikadevaraju@gmail.com>
2026-04-30 06:10:06 +0530

    Add files via upload

---
+++ AI_Speech_to_text/speechtotext.py
@@ -0,0 +1,12 @@
+#pip install pyaudio
+#pip install SpeechRecognition
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said :", text)
+    except:
+        print("Sorry, could not recognize your voice")
